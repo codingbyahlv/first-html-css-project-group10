d1baabb43f1da12b3bd367c2b77cff453d5500bd
Ann-Helen Lausmaa <code@ahlvdesign.se>
2021-09-21 14:53:05 +0200

    mobile menu, responsive first page

--- script/main.js
+++ script/main.js
@@ -1,3 +1,34 @@
+let menuOpen = false;
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".mobile-menu");
+const links = document.getElementsByClassName("mobile-link");
+const documentBody = document.body;
+
 function onSubmit(){
     alert("Tack för ditt meddelande. Vi återkommer så snart vi kan");
 }
+
+function toggleMenu() {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+      menu.style.right = "0%";
+      documentBody.style.overflow = "hidden";
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+      menu.style.right = "-100%";
+      documentBody.style.overflow = "auto";
+    }
+  }
+  
+  window.addEventListener("load", function () {
+    menuBtn.addEventListener("click", function () {
+      toggleMenu();
+    });
+    for(let i = 0; links.length; i++){
+      links[i].addEventListener("click", function(){
+        toggleMenu();
+      });
+    }
+  });
